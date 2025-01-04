8b2b4529d95655140cf1fe0dd92ea2503d0305bf
Sean Cravener <scravenerpat@gmail.com>
2025-01-04 01:17:06 -0500

    Added custom title image I made to home page frame. Added shadow to the title frame border.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,21 @@ import Frame from "../components/ui/Frame/Frame";
 import "./Home.css";
 
 const Home = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={`home ${theme}`}>
       <div className="home-header">
         <Frame>
-          <h1>
+          <img src="/Asset-2smaller.png" alt="logo" />
+          {/* <h1>
             <span>S</span>ean <span>C</span>
             ravener's
           </h1>
           <h1>Portfolio</h1>
           <button onClick={() => setTheme(theme === "bg" ? "gradient" : "bg")}>
             Switch Theme
-          </button>
+          </button> */}
         </Frame>
       </div>
     </div>
